1f8ab05f1178f208e7b4f9abaec83a35b888f5c0
duyot <ongtheduy@gmail.com>
2016-09-16 17:21:26 +0700

    #3 - update support rest service - init db util for creating connection

---
+++ src/main/java/com/vivas/persistent/dao/DBUtils.java
@@ -0,0 +1,31 @@
+package com.vivas.persistent.dao;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+/**
+ * Created by duyot on 9/16/2016.
+ */
+public class DBUtils {
+    public static Logger log = LoggerFactory.getLogger(DBUtils.class);
+
+    public static Connection getConnection(){
+        Context initContext = null;
+        try {
+            initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:comp/env");
+            DataSource ds = (DataSource) envContext.lookup("jdbc/ConnectionPool");
+            return ds.getConnection();
+        } catch (Exception e) {
+            log.error("Get connection fail caused: "+ e.toString());
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
